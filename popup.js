65a890d287d18c22e9ea3a7f5a3507e15667b702
Tatsuya Kosuge <castero1219@gmail.com>
2024-02-22 12:14:05 +0900

    [add]ソースコード修正

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 document
-  .getElementById("toggleVisualizer")
+  .querySelector("#toggleVisualizer")
   .addEventListener("change", function (event) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { visualize: event.target.checked });
